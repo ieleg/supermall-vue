9e43cf7734ef0516722f3dc897aec077f38f5ffd
lei <1327095055@qq.com>
2020-03-13 17:54:44 +0800

    recmmend

---
+++ src/views/home/childComponent/homeRecommend.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="recommend">
+    <div class="recommendItem" v-for="item in recommend" :key="item.image">
+      <a :href='item.link'>
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    recommend:{
+      type: Array,
+      default(){
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .recommend{
+    display: flex;
+    width: 100%;
+    /* margin:0 auto; */
+    text-align: center;
+    font-size: 12px;
+    padding-top: 8px;
+    padding-bottom: 15px;
+    box-shadow: 5px 0 3px rgba(100, 100, 100, 0.8);
+  }
+  .recommendItem{
+    flex: 1;
+    /* margin:0 auto; */
+  }
+  .recommend img{
+    width: 68px;
+    height: 68px;
+    margin-bottom: 8px;
+  }
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,6 +4,7 @@
       <div slot="center">购物街</div>
     </navbar>
      <homeSwiper :banner="banner" />
+     <recommend :recommend='recommend' />
   </div>
 </template>
 
@@ -11,10 +12,12 @@
 import {getHomeMultidata} from '@/network/home'
 import navbar from '@/components/common/navbar/navbar'
 import homeSwiper from './childComponent/homeSwiper'
+import recommend from './childComponent/homeRecommend'
 export default {
   components:{
     navbar,
-    homeSwiper
+    homeSwiper,
+    recommend
   },
   data(){
     return {
@@ -27,7 +30,7 @@ export default {
       // console.log(res.data); 
       this.banner = res.data.data.banner.list;
       this.recommend = res.data.data.recommend.list
-      console.log(this.banner);
+      console.log(this.recommend);
       
     })
   }
